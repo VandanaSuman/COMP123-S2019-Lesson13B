91f1df97cb50b7e1c4fbd3d8126cd72964673a3b
301045522 <301045522@PR-E216-17>
2019-08-07 16:01:01 -0400

    Added multiple projects.

---
+++ COMP123-S2019-Lesson13B-PartC/Views/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lesson13B_PartC.Views
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+          if (NameTabControl.SelectedIndex <NameTabControl.TabPages.Count - 1)
+            { 
+            NameTabControl.SelectedIndex++;
+            }
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            if(NameTabControl.SelectedIndex != 0)
+            { 
+            NameTabControl.SelectedIndex--;
+            }
+        }
+    }
+}
